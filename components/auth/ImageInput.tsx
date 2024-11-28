f1fda7793564f7390745ce2de504da36cf83cc77
Daddypastor <emmaexcellent030@gmail.com>
2024-11-28 03:33:41 +0100

    updated

--- components/auth/ImageInput.tsx
+++ components/auth/ImageInput.tsx
@@ -1,46 +1,23 @@
-import React, { useState } from 'react';
-import Image from 'next/image';
-
+import React, { useState } from "react";
+import Image from "next/image";
 
 const ImageInput = () => {
-  const [preview, setPreview] = useState('/testimonial/test2.png');
-  const [file, setFile] = useState(null);
+  const [preview, setPreview] = useState("/testimonial/test2.png");
+  const [file, setFile] = useState<File | null>(null);
 
-  const handleImageChange = (e: { target: { files: any[]; }; }) => {
-    const selectedFile = e.target.files[0];
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = e.target.files?.[0]; // Safely access the file
     if (selectedFile) {
       setPreview(URL.createObjectURL(selectedFile));
       setFile(selectedFile);
     }
   };
 
-  // const handleSave = async () => {
-  //   if (file) {
-  //     try {
-  //       const response = await storage.createFile('YOUR_BUCKET_ID', file);
-  //       console.log('File uploaded successfully', response);
-        
-  //       // Optionally, save file information to a database collection
-  //       await databases.createDocument('YOUR_DATABASE_ID', 'YOUR_COLLECTION_ID', {
-  //         fileId: response.$id,
-  //         fileUrl: response.$url,
-  //       });
-
-  //       alert('Image saved successfully!');
-  //     } catch (error) {
-  //       console.error('Error uploading file', error);
-  //       alert('Failed to save image!');
-  //     }
-  //   } else {
-  //     alert('No image selected!');
-  //   }
-  // };
-
   return (
     <div className="flex flex-col justify-center items-center gap-3 pt-3">
       <div className="relative w-28 h-28 rounded-full overflow-hidden shrink-0">
         <Image
-          id='preview_img'
+          id="preview_img"
           width={120}
           height={120}
           className="absolute inset-0 w-full h-full object-cover rounded-full"
@@ -52,6 +29,7 @@ const ImageInput = () => {
         <span className="sr-only">Choose profile photo</span>
         <input
           type="file"
+          accept="image/*"
           onChange={handleImageChange}
           className="cursor-pointer text-sm w-56 text-slate-500
             file:mr-4 file:py-2 file:px-4
@@ -63,6 +41,6 @@ const ImageInput = () => {
       </label>
     </div>
   );
-}
+};
 
 export default ImageInput;
